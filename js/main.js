6ec246c5d4ad9ee76d436528d8c2e2aa8c8a3ffc
Saqif Badruddin <saqif280@gmail.com>
2018-01-25 11:01:03 -0500

    new pages

--- js/main.js
+++ js/main.js
@@ -16,3 +16,25 @@ $('.switch-lang').click(function() {
   $('[lang="es"]').toggle();
   $('[lang="en"]').toggle();
 });
+
+// NAVBAR ANIMATIONS
+var rgbInvScroll = (255 - Math.round(($(document).scrollTop()/$(window).height())*255));
+if($(window).scrollTop() > $(window).height()){
+  $('.navbar').css({'background-color':'rgba(255,255,255,0.9)'}); 
+  $('.nav-text').css({'color':'black'});
+}
+else{
+  $('.navbar').css({'background-color':'rgba(255,255,255,0)'});
+  // $('.nav-text').css({'color':'rgb('+rgbInvScroll+','+rgbInvScroll+','+rgbInvScroll+')'});
+}
+$(window).scroll(function(){
+	var rgbInvScroll = (255 - Math.round(($(document).scrollTop()/$(window).height())*255));
+  if($(window).scrollTop() > $(window).height()){
+    $('.navbar').css({'background-color':'rgba(255,255,255,0.9)'});
+    $('.nav-text').css({'color':'black'});
+  }
+  else{
+    $('.navbar').css({'background-color':'rgba(255,255,255,'+($(document).scrollTop()/$(window).height())*0.9+')'});
+    // $('.nav-text').css({'color':'rgb('+rgbInvScroll+','+rgbInvScroll+','+rgbInvScroll+')'});
+  }
+});
